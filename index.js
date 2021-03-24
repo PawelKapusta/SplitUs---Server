c2a7cdc8956c3a052fb63a0f5014aff63cb88b7c
Legion <pawel.kapusta@student.uj.edu.pl>
2021-03-24 21:31:57 +0100

    Change database connection

--- index.js
+++ index.js
@@ -11,34 +11,51 @@ require('dotenv').config();
 app.use(cors());
 app.use(bodyParser.json());
 
-const db = mysql.createConnection({
+const db_config = {
   host: process.env.DB_HOST,
-  user: process.env.DB_USER ,
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB,
-});
+};
+let connection;
 
-db.connect((error) => {
-  if (error) {
-    console.log('Database connection error');
-  } else {
-    console.log('Database connected');
-  }
-});
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function (err) {
+    if (err) {
+      console.log('Database connection error:', err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log('Database connected');
+    }
+  });
+
+  connection.on('error', function (err) {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 app.get('/', (req, res) => {
   res.send('<h1>Server side</h1>');
 });
-app.get('/users',  (req,res) => {
-  db.query("SELECT * FROM users;", (error, rows, fields) => {
+app.get('/users', (req, res) => {
+  connection.query('SELECT * FROM users;', (error, rows, fields) => {
     if (error) {
-      console.log("Error in getting users query");
+      console.log('Error in getting users query');
     } else {
       res.send(rows);
-      console.log("Successful getting users query");
+      console.log('Successful getting users query');
     }
-  })
-})
+  });
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Running on port 5000');
